e08c728d578a7bbcf747a949ee2c74a4a3e1e4ed
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2019-06-13 23:56:27 +0200

    Added minor style changes

--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -1,3 +1,11 @@
 <div class="navbar bg-success">
-    <h1>ScoreBoard App with SVELTE.js</h1>
+    <h1>ScoreBoard App with</h1>
+    <img src="img/svelte-logo-horizontal.svg" alt="Logo">
 </div>
+
+<style>
+    img {
+        width: 20%;
+        margin-left: 1rem;
+    }
+</style>
